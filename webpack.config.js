8aeb38194960c8684c0266004e2530b7178ccfbf
iandavall <iduality5n@gmail.com>
2020-06-24 11:19:34 -0400

    Modified webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 /* Require `path` */
 const path = require("path");
-/* Require `webpack` */
+/* Require Webpack */
 const webpack = require("webpack");
-/* Populate module exports */
-module.exports = {
 
+/* Populate module exports to use with Webpack */
+module.exports = {
+    entry: "./src/client/index.js",
 }
