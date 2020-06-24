3c3b660df9c82495626815a553bc782bdc821e3e
iandavall <iduality5n@gmail.com>
2020-06-24 19:09:30 -0400

    Updated scripts in 'package.json' to distinguish between Production and Developments environments, created a Webpack Production config file, and renamed generic Webpack config file into Development one

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,35 +1,34 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+/* Require `path` */
+const path = require('path');
+/* Require Webpack */
+const webpack = require('webpack');
+/* Require HTMLWebPackPlugin */
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
+/* Populate module exports to use with Webpack */
 module.exports = {
+    mode: 'development', /* Add mode flag for Development */
+    devtool: 'source-map', /* Add Source Map devtool to generate source mapping */
     entry: './src/client/index.js',
-    mode: 'development',
-    devtool: 'source-map',
-    stats: 'verbose',
     module: {
         rules: [
             {
+                enforce: 'pre',
                 test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
+                exclude: '/node_modules/',
+                loader: 'eslint-loader',
+            },
+            {
+                test: '/\.js$/',
+                exclude: '/node_modules/',
+                loader: 'babel-loader',
             }
         ]
     },
     plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        }),
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
-            dry: true,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
+        new HtmlWebPackPlugin({ /* Declare new instance of HTMLWebPackPlugin */
+            template: './src/client/views/index.html', /* Telling it to look at HTML file in '/client/views' */
+            filename: './index.html', /* Telling it to generate a new 'index.html' file inside of the 'dist' folder */
         })
     ]
-}
+};
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,35 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+/* Require `path` */
+const path = require("path");
+/* Require Webpack */
+const webpack = require("webpack");
+/* Require HTMLWebPackPlugin */
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
+/* Populate module exports to use with Webpack */
 module.exports = {
-    entry: './src/client/index.js',
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        })
-    ]
-}
+  mode: "production",
+  entry: "./src/client/index.js",
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: "/.js$/",
+        exclude: "/node_modules/",
+        loader: "eslint-loader",
+      },
+      {
+        test: "/.js$/",
+        exclude: "/node_modules/",
+        loader: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template:
+        "./src/client/views/index.html",
+      filename:
+        "./index.html",
+    }),
+  ],
+};
